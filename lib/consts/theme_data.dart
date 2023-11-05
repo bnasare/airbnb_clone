19c32d1bab4b87e7ea460e63f753daa7cf507ed9
bnasare <124492150+bnasare@users.noreply.github.com>
2023-11-05 22:41:33 +0000

    UI

--- lib/consts/theme_data.dart
+++ lib/consts/theme_data.dart
@@ -3,13 +3,14 @@ import 'package:google_fonts/google_fonts.dart';
 
 ThemeData themeData() {
   return ThemeData(
-      colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.black,
-          primary: Colors.black,
-          secondary: Colors.red[900],
-          tertiary: Colors.black.withOpacity(0.4)),
-      textTheme: GoogleFonts.nunitoSansTextTheme(),
-      appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.white,
-      ));
+    colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.black,
+        primary: Colors.black,
+        secondary: Colors.red[900],
+        tertiary: Colors.black.withOpacity(0.4)),
+    textTheme: GoogleFonts.nunitoSansTextTheme(),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+    ),
+  );
 }
